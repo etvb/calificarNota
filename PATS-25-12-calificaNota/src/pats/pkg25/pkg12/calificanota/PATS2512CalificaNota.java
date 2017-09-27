e1112af57855a65f6ee1309c9d597afe00e75147
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 07:17:33 -0500

    archivos base

---
+++ PATS-25-12-calificaNota/src/pats/pkg25/pkg12/calificanota/PATS2512CalificaNota.java
@@ -0,0 +1,14 @@
+/*
+Ejercicio 12: Pedir una nota de 0 a 10 y mostrarla de la forma: Insuficiente,
+Suficiente, Bien, Notable, Sobresaliente
+*/
+package pats.pkg25.pkg12.calificanota;
+
+public class PATS2512CalificaNota {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
