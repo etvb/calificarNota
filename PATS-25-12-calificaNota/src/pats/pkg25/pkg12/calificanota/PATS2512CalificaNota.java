b4154369f1302a90a920352cdda1a65e4ab1a294
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 07:31:32 -0500

    resolvemos el problema con switch

--- PATS-25-12-calificaNota/src/pats/pkg25/pkg12/calificanota/PATS2512CalificaNota.java
+++ PATS-25-12-calificaNota/src/pats/pkg25/pkg12/calificanota/PATS2512CalificaNota.java
@@ -14,6 +14,27 @@ public class PATS2512CalificaNota {
         
         nota = Short.parseShort(JOptionPane.showInputDialog("ingresa la Calificacion"));
         
+        switch(nota){
+            case 10:
+                JOptionPane.showMessageDialog(null, "SOBRESALIENTE");
+                break;
+            case 9:
+                JOptionPane.showMessageDialog(null, "NOTABLE");
+                break;
+            case 8: case 7:
+                JOptionPane.showMessageDialog(null, "BIEN");
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null, "SUFICIENTE");
+                break;
+            case 5: case 4: case 3: case 2: case 1:
+                JOptionPane.showMessageDialog(null, "INSUFICIENTE");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "NO es un numero entre 1 y 10");
+                break; //este break pude o no puede ir.
+        }
+        
     }
     
 }
