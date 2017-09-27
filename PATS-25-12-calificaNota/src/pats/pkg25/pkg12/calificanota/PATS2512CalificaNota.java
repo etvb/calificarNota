1a1ac65a7a79495b6fdd239b2890082eede2c765
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 07:21:39 -0500

    inicializamos la variable nota

--- PATS-25-12-calificaNota/src/pats/pkg25/pkg12/calificanota/PATS2512CalificaNota.java
+++ PATS-25-12-calificaNota/src/pats/pkg25/pkg12/calificanota/PATS2512CalificaNota.java
@@ -4,10 +4,15 @@ Suficiente, Bien, Notable, Sobresaliente
 */
 package pats.pkg25.pkg12.calificanota;
 
+import javax.swing.JOptionPane;
+
 public class PATS2512CalificaNota {
 
     public static void main(String[] args) {
         
+        short nota;
+        
+        nota = Short.parseShort(JOptionPane.showInputDialog("ingresa la Calificacion"));
         
     }
     
